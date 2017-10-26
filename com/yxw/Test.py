d6cf9690d94c6e00998b761b4600a70664699a1e
git <16916962@163.com>
2017-10-26 17:05:59 +0800

    Initial commit

---
+++ com/yxw/Test.py
@@ -0,0 +1,13 @@
+# name = input('What is your first name?')
+# print('Hello '+name.capitalize()+'!')
+
+# for i in range(10,0,-2):
+#     print(i)
+
+i = 0
+while i < 10:
+    print(i)
+    i += 1
+    if i % 2 == 0 and i != 0:
+        break
+print("\n end")
